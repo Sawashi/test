9a4ba7dac152eec86e47097c9a2999ef9448f1e4
Kiet <92447748+Sawashi@users.noreply.github.com>
2024-12-26 22:17:42 +0700

    layout for player info

--- src/pages/home/playerInfo/index.js
+++ src/pages/home/playerInfo/index.js
@@ -2,7 +2,81 @@ import React from "react";
 import "./style.css";
 
 const PlayerInfo = () => {
-	return <div className="cardInfo">hahahaha</div>;
+	return (
+		<div className="cardInfo">
+			<div className="parent">
+				<div className="child">
+					<div class="card text-center">
+						<div class="card-header">Featured</div>
+						<div class="card-body">
+							<div className="col align-items-start">
+								<div className="name">name</div>
+								<div className="logo-info">
+									<div className="logo">logo</div>
+									<div className="info">info</div>
+								</div>
+							</div>
+						</div>
+						<div class="card-footer">footer</div>
+					</div>
+				</div>
+				<div className="child row">
+					<div className="row">
+						<div className="col align-items-start">
+							<div className="name">name</div>
+							<div className="logo-info">
+								<div className="logo">logo</div>
+								<div className="info">info</div>
+							</div>
+						</div>
+
+						<div className="col align-items-start">
+							<div className="name">name</div>
+							<div className="logo-info">
+								<div className="logo">logo</div>
+								<div className="info">
+									<div>a</div>
+									<div>b</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="row">
+						<div className="col align-items-start">
+							<div className="name">name</div>
+							<div className="logo-info">
+								<div className="logo">logo</div>
+								<div className="info">info</div>
+							</div>
+						</div>
+						<div className="col align-items-start">
+							<div className="name">name</div>
+							<div className="logo-info">
+								<div className="logo">logo</div>
+								<div className="info">info</div>
+							</div>
+						</div>
+					</div>
+					<div className="row">
+						<div className="col align-items-start">
+							<div className="name">name</div>
+							<div className="logo-info">
+								<div className="logo">logo</div>
+								<div className="info">info</div>
+							</div>
+						</div>
+						<div className="col align-items-start">
+							<div className="name">name</div>
+							<div className="logo-info">
+								<div className="logo">logo</div>
+								<div className="info">info</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default PlayerInfo;
