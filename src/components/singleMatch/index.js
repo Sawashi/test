e6bdb49e70ca52a6a4ad9b04e65e075fad4f72a8
Kiet <92447748+Sawashi@users.noreply.github.com>
2024-12-27 16:15:59 +0700

    matches

---
+++ src/components/singleMatch/index.js
@@ -0,0 +1,150 @@
+import React from "react";
+import "./style.css";
+import teamMatch from "../../assets/Source Images-Icons/Icons/team.webp";
+import backupSymbol from "../../assets/Source Images-Icons/Icons/backup.png";
+const SingleMatch = ({
+	dateTime,
+	matchStatus,
+	homeTeam,
+	awayTeam,
+	homeTeamScore,
+	awayTeamScore,
+}) => {
+	const convertDate = (inputTimestamp) => {
+		// Convert the input to a number
+		const timestamp = Number(inputTimestamp);
+
+		// Validate the timestamp
+		if (isNaN(timestamp)) {
+			throw new Error(
+				"Invalid timestamp: Input must be a valid number or numeric string."
+			);
+		}
+
+		// Convert the timestamp to milliseconds and create a Date object
+		const date = new Date(timestamp * 1000);
+
+		// Extract the day and month
+		const day = date.getUTCDate(); // For UTC time
+		const month = date.getUTCMonth() + 1; // Months are zero-based
+
+		return day + "/" + month;
+	};
+	const mapStatus = new Map([
+		["live", "Live"],
+		["finished", "FT"],
+		["not started", "NS"],
+		["after extra time", "AET"],
+	]);
+	const mapTeamAndNation = new Map([
+		["UD Las Palmas", "Spain"],
+		["Girona FC", "Spain"],
+		["Real Sociedad", "Spain"],
+		["Real Betis", "Spain"],
+		["Cadiz", "Spain"],
+		["Deportivo Alavés", "Spain"],
+		["Manchester City", "England"],
+		["Chelsea", "England"],
+		["Celtic FC", "Scotland"],
+		["AC Milan", "Italy"],
+		["Como", "Italy"],
+		["Cagliari", "Italy"],
+		["Udinese", "Italy"],
+		["Bologna", "Italy"],
+		["Atalanta", "Italy"],
+		["Hellas Verona", "Italy"],
+		["Napoli", "Italy"],
+		["Parma", "Italy"],
+		["Torino", "Italy"],
+		["FC Barcelona", "Spain"],
+	]);
+	const timeToShow = convertDate(dateTime);
+	const homeToShow = mapTeamAndNation.get(homeTeam);
+	const awayToShow = mapTeamAndNation.get(awayTeam);
+	console.log(matchStatus);
+	return (
+		<div className="matchInfo">
+			<div className="d-flex align-items-center justify-content-between ">
+				<div>
+					<div className="row">
+						<div className="col-4">
+							<div>{timeToShow}</div>
+							<div>{mapStatus.get(matchStatus)}</div>
+						</div>
+						<div className="col single_match_info">
+							<div className="logo-info">
+								<div className="info">
+									<div>
+										<span>
+											<img
+												src={teamMatch}
+												alt="spain"
+												className="rounded small_flag"
+											/>
+										</span>
+										{homeToShow}
+									</div>
+									<div>
+										<span>
+											<img
+												src={teamMatch}
+												alt="spain"
+												className="rounded small_flag"
+											/>
+										</span>
+										{awayToShow}
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="rightSide">
+					<div className="row">
+						<div className="col single_match_info">
+							<div className="logo-info">
+								<div className="info_score">
+									<div>
+										<div
+											className={
+												"topScore " +
+												(homeTeamScore > awayTeamScore
+													? "winScore"
+													: homeTeamScore < awayTeamScore
+													? "topLose"
+													: "drawScore")
+											}
+										>
+											{homeTeamScore}
+										</div>
+										<div
+											className={
+												"bottomScore " +
+												(homeTeamScore > awayTeamScore
+													? "bottomLose"
+													: homeTeamScore < awayTeamScore
+													? "winScore"
+													: "drawScore")
+											}
+										>
+											{awayTeamScore}
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div className="col-4">
+							<img
+								src={backupSymbol}
+								alt="spain"
+								className="rounded small_flag"
+							/>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default SingleMatch;
